92c05340ca9085a265929da7e7b90d2648a74908
davidcodes17 <areegbedavid@gmail.com>
2024-03-05 13:41:05 +0100

    fix:Responsiveness

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
     <Box mt={10}>
       <Flex justifyContent={'space-between'} flexWrap={{lg : "nowrap", md : "nowrap", sm : "wrap", base : "wrap"}} alignItems={'center'}>
         <Box width={{lg : "40vw", md : "40vw", sm : "100%", base : "100%"}}>
-          <Heading fontSize={{lg : 70, md : 70, sm : "40px", base: "40px"}}>
+          <Heading fontSize={{lg : 70, md : 70, sm : "50px", base: "40px"}}>
             Navigating the digital landscape for success
           </Heading>
-          <Text fontSize={{lg : 70, md : 70, sm : "50px", base : "12px"}}>
+          <Text fontSize={{lg : 20, md : 20, sm : "15px", base : "12px"}}>
             Our digital marketing agency helps businesses grow and succeed
             online through a range of services including SEO, PPC, social media
             marketing, and content creation.
